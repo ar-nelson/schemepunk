85740e92befb2c8270225afbfba1f2b060cf8f70
Adam R. Nelson <adam@nels.onl>
2020-04-08 22:25:43 -0400

    fix test-larceny script

--- scripts/test-larceny.sh
+++ scripts/test-larceny.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-TEST_FILES="$(echo "$@" | sort | awk '{printf "(load \"%s\") ", $0}')"
-echo "(import (scheme load)) $TEST_FILES (end-test-runner)" | larceny -r7 -I .
+TEMP_SCRIPT="$(mktemp)"
+echo "(import (scheme load) (schemepunk test))" >> $TEMP_SCRIPT
+echo "$@" | sort | awk '{printf "(load \"%s\") ", $0}' >> $TEMP_SCRIPT
+echo "(end-test-runner)" >> $TEMP_SCRIPT
+larceny -r7 -I . "$TEMP_SCRIPT"
+rm "$TEMP_SCRIPT"
