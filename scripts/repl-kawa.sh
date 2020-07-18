239ec29d7c90b401fa0702c2e525f5701cb01b32
Adam R. Nelson <adam@nels.onl>
2020-07-18 18:09:44 -0400

    fix repl-kawa script

--- scripts/repl-kawa.sh
+++ scripts/repl-kawa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 with_f () {
-  awk '{printf "-f %s ", $0}'
+  tr " " "\n" | awk '{printf "-f %s ", $0}'
 }
 
 kawa --full-tailcalls $("$(dirname "$0")/find-dependencies.scm" "$(dirname "$0")/repl.scm" | with_f) -f "$(dirname "$0")/repl.scm" -s
