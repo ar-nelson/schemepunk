ad895ac7c803c787e6858392e3ef419a7f98105e
Adam R. Nelson <adam@nels.onl>
2020-10-04 00:07:19 -0400

    clear Sagittarius cache before running tests

--- scripts/test-sagittarius.sh
+++ scripts/test-sagittarius.sh
@@ -3,5 +3,5 @@ TEMP_SCRIPT="$(mktemp)"
 echo "(import (scheme load) (schemepunk test))" >> $TEMP_SCRIPT
 echo "$@" | sort | awk '{printf "(load \"%s\") ", $0}' >> $TEMP_SCRIPT
 echo "(end-test-runner)" >> $TEMP_SCRIPT
-sagittarius -r7 -L. "$TEMP_SCRIPT"
+sagittarius -c -r7 -L. "$TEMP_SCRIPT"
 rm "$TEMP_SCRIPT"
